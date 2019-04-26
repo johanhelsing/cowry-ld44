d95c429e69e6014f1e3ea3b29cba0326d22f0d59
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2019-04-26 19:53:18 +0200

    Add rotating cube

---
+++ Assets/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float xSpeed = 0;
+    public float ySpeed = 0;
+    public float zSpeed = 100;
+    void Update()
+    {
+        float dt = Time.deltaTime;
+        transform.Rotate(xSpeed * dt, ySpeed * dt, zSpeed * dt);
+    }
+}
